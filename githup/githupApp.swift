21f0fd73f45a13f0525086865be5ce87e05bde6a
Aisha <alkabiaisha73@gmail.com>
2023-07-27 00:56:34 +0400

    Initial Commit

---
+++ githup/githupApp.swift
@@ -0,0 +1,17 @@
+//
+//  githupApp.swift
+//  githup
+//
+//  Created by عائشة محمد الكعبي on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct githupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
